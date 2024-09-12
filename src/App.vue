37f3e70a1f8cc9af50ad39008bd3072fc763ae8e
maxbelogurov <maxbelogurov@gmail.com>
2024-09-12 13:55:07 +0200

    added: transition for routers

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <header-main />
-  <RouterView />
+  <router-view v-slot="{Component}">
+    <transition name="page" mode="out-in">
+      <component :is="Component" :key="Component"/>
+    </transition>
+  </router-view>
 </template>
 
 <script setup>
@@ -22,6 +26,14 @@ mainStore.setMobile(isMobile);
 
 </script>
 
-<style>
+<style lang="scss">
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.3s ease;
+}
 
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
 </style>
